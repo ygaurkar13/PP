5bad24dfe6a7086739b5ff2999cab6c40e34addc
ygaurkar13 <155806647+ygaurkar13@users.noreply.github.com>
2024-08-21 21:15:24 +0530

    Practical_4

--- change_case.py
+++ change_case.py
@@ -42,7 +42,50 @@ print(change_case(a,"r"))
 print(change_case(a,"R"))
 
 
+def change_case1(string,style):
+	if style in ("Z","z"):
+		y=[]
+		for i,ele in enumerate(string):
+			if "A"<=ele<="Z":
+				if i%2==0:
+					y.append(ele if "A"<=string[0]<="Z" else chr(ord(ele)+32))
+				else:
+					y.append(chr(ord(ele)+32) if "A"<=string[0]<="Z" else ele)
+			elif "a"<=string[0]<="z":
+				if i%2==0:
+					y.append(ele if "a"<=string[0]<="z" else chr(ord(ele)-32))
+				else:
+					y.append(chr(ord(ele)-32) if "a"<=string[0]<="z" else ele)
+	else:
+		y.append(ele)
+	return "".join(y)
+s="AbCdefg"
+print(change_case1(s,"Z"))
+print(change_case1(s,"z"))
 
+
+
+def change_case1(string,style):
+	if style =="zigzag2":
+		y=[]
+		for i,ele in enumerate(string):
+			if "A"<=ele<="Z":
+				if i%2==0:
+					y.append(chr(ord(ele)+32))
+				else:
+					y.append(ele)
+			elif "a"<=string[0]<="z":
+				if i%2==0:
+					y.append(ele)
+				else:
+					y.append(chr(ord(ele)-32))
+			else:
+				y.append(ele)
+	else:
+		return "Invalid Syntax"
+	return "".join(y)
+s="abcdef"
+print(change_case1(s,"zigzag2"))
 
 
 
