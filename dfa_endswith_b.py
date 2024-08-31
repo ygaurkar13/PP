01445d96c37efd86ceaa4c9e029475714e16205f
ygaurkar13 <155806647+ygaurkar13@users.noreply.github.com>
2024-08-31 13:27:22 +0530

    Practical_5

---
+++ dfa_endswith_b.py
@@ -0,0 +1,30 @@
+def S0(text):
+	if text[0]=="a":
+		if len(text)==1:
+			return "rejected"
+		else:
+			return S0(text[1:])
+	elif text[0]=="b":
+		if len(text)==1:
+			return "accepted"
+		else:
+			return S1(text[1:])
+	else:
+		return "rejected"
+			
+def S1(text):
+	if text[0]=="b":
+		if len(text)==1:
+			return "accepted"
+		else:
+			return S0(text[1:])
+	elif text[0]=="a":
+		if len(text)==1:
+			return "rejected"
+		else:
+			return S1(text[1:])
+	else:
+		return "accepted"	
+	
+text="abbbb"
+print(S0(text))	
